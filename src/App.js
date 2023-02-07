61a919a86a4552a7507ed01dce835f03e2bf43d1
AnaDzonicc <ana.dzonicc.022@gmail.com>
2023-02-07 08:45:23 -0800

    style issue

--- src/App.js
+++ src/App.js
@@ -447,7 +447,7 @@ function App() {
                   </div>
                 </div>
               ) : (
-                <div className="flex items-center rounded-xl border-[1px] border-[#ccc] mx-8 py-2 my-1">
+                <div className="flex items-center rounded-xl border-[1px] border-[#ccc] md:mx-20 mx-3 py-2 my-1">
                   <div className="ml-3" onClick={() => setSearchBox(false)}>
                     <SearchIcon />
                   </div>
@@ -577,29 +577,8 @@ function App() {
             </svg>
           </button>
         </div>
+
         <div className="w-full hidden md:block  md:w-[20%] px-4 py-10 bg-[#f1f1f1] fixed h-full right-0">
-          <h1 className="text-3xl font-bold text-center">Warenkorb</h1>
-          <div className="flex justify-center mt-10">
-            <svg
-              viewBox="0 0 16 16"
-              className="w-12 h-12"
-              role="presentation"
-              focusable="false"
-              aria-hidden="true"
-            >
-              <path
-                d="M12.996 4.719h-2.371V2.53L9.313 1.22H6.688L5.375 2.53V4.72H3.004l-.429 8.452a1.523 1.523 0 001.531 1.61h7.788a1.522 1.522 0 001.531-1.61l-.429-8.452zM6.688 2.53h2.625V4.72H6.688V2.53zM12.05 13.4a.219.219 0 01-.157.07H4.106a.228.228 0 01-.218-.219l.358-7.21h7.508l.359 7.21a.22.22 0 01-.062.149z"
-                fill="#000"
-              ></path>
-            </svg>
-          </div>
-          <h1 className="text-2xl font-bold text-center mt-2">
-            Fülle deinen Warenkorb
-          </h1>
-          <p className="text-md text-center mt-1">
-            Füge einige leckere Gefrichte aus der Speisekarte hinzu und bestelle
-            dein Essen.
-          </p>
           <CartList />
         </div>
       </div>
